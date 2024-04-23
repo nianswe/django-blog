a6a6329f0119ccd67578e84fde0e3545a22ae7a0
Niklas Andersson <nianswe@hotmail.com>
2024-04-23 13:40:53 +0000

    Prepar for Heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(50t$cb098p@+mja7w7x1ibhqfzwdb$h@exd#+y82er4eorj%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
