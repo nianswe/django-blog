6fd34a469e941ed6b2f372f96ab58c3b30330d1a
Niklas Andersson <nianswe@hotmail.com>
2024-04-24 10:25:25 +0000

    Setup SQL DB Security

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(50t$cb098p@+mja7w7x1ibhqfzwdb$h@exd#+y82er4eorj%l'
+# SECRET_KEY = ''
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
