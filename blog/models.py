b51e90cb3f3da3ca74007595a53f76e41878efc5
Niklas Andersson <nianswe@hotmail.com>
2024-04-26 14:46:39 +0000

    zero

--- blog/models.py
+++ blog/models.py
@@ -36,30 +36,8 @@ class Comment(models.Model):
     approved = models.BooleanField()
     created_on = models.DateTimeField(auto_now_add=True)
 
-    challenge = models.FloatField(default=3.0)
-
     class Meta:
         ordering = ["-created_on"]
     
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-    
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
-    )    
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
-    body = models.TextField()
-    approved = models.BooleanField()
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    challenge = models.FloatField(default=3.0)
-
-    class Meta:
-        ordering = ["-created_on"]
-    
-    def __str__(self):
-        return f"Comment {self.body} by {self.author}"
-
