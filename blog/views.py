42b4b9b029f1fcb4a22056497b3db10e4dfdac50
Niklas Andersson <nianswe@hotmail.com>
2024-04-26 13:57:20 +0000

    added comments2

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.views import generic
+from django.contrib import messages
 from .models import Post
-# from .models import Event
+from .forms import CommentForm
 
 # Create your views here.
-# def my_blog(request):
-#    return HttpResponse("Hello, Blog!")
+
 
 class PostList(generic.ListView):
-
-
     queryset = Post.objects.filter(status=1)
-    # template_name = "post/index.html"
     template_name = "blog/index.html"
-    # model = Event
-    # template_name = "index.html"
-    paginate_by = 12
-
-
-# def event_detail(request, event_id):
+    paginate_by = 6
 
 
 def post_detail(request, slug):
@@ -39,5 +30,27 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-    return render(request, "blog/post_detail.html", {"post": post, "comments": comments,
-        "comment_count": comment_count,} )
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
+        },
+    )
